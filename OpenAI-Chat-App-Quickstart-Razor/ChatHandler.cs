c6a71a934cf9a90a26a745a482d9b6eb2633d9c7
Jordan Matthiesen <1333029+jmatthiesen@users.noreply.github.com>
2024-09-04 22:41:52 -0700

    Add project files.

---
+++ OpenAI-Chat-App-Quickstart-Razor/ChatHandler.cs
@@ -0,0 +1,119 @@
+﻿using Azure.Core.Diagnostics;
+using Azure.Identity;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using System;
+
+public class Message
+{
+    public string Role
+    {
+        get; set;
+    }
+
+    public string Content
+    {
+        get; set;
+    }
+}
+internal class ChatHandler
+{
+    public ChatHandler()
+    {
+    }
+
+    internal async Task<Message> Chat(List<Message> messages)
+    {
+        IKernelBuilder builder = Kernel.CreateBuilder();
+        InitBuilder(builder);
+
+        Kernel kernel = builder.Build();
+
+        ChatHistory history = new ChatHistory("You are a helpful assistant.");
+        foreach (Message message in messages)
+        {
+            history.AddMessage(new AuthorRole(message.Role), message.Content);
+        }
+
+        IChatCompletionService chat = kernel.GetRequiredService<IChatCompletionService>();
+        ChatMessageContent response = await chat.GetChatMessageContentAsync(history);
+
+        return new Message()
+        {
+            Role = response.Role.ToString(),
+            Content = (response.Items[0] as TextContent).Text
+        };
+    }
+
+    internal async IAsyncEnumerable<string> Stream(List<Message> messages)
+    {
+        IKernelBuilder builder = Kernel.CreateBuilder();
+        InitBuilder(builder);
+
+        Kernel kernel = builder.Build();
+
+        ChatHistory history = new ChatHistory("You are a helpful assistant.");
+        foreach (Message message in messages)
+        {
+            history.AddMessage(new AuthorRole(message.Role), message.Content);
+        }
+
+        IChatCompletionService chat = kernel.GetRequiredService<IChatCompletionService>();
+        IAsyncEnumerable<StreamingChatMessageContent> response = chat.GetStreamingChatMessageContentsAsync(history);
+
+        await foreach (StreamingChatMessageContent content in response)
+        {
+            yield return (content.Content);
+        }
+    }
+
+    private static void InitBuilder(IKernelBuilder builder)
+    {
+        string openai_host = Environment.GetEnvironmentVariable("OPENAI_HOST");
+
+        if (String.IsNullOrEmpty(openai_host))
+        {
+            openai_host = "openai";
+        }
+
+        if (openai_host.Equals("local", StringComparison.OrdinalIgnoreCase))
+        {
+            string endpoint = Environment.GetEnvironmentVariable("LOCAL_MODELS_ENDPOINT");
+            if (!String.IsNullOrEmpty(endpoint))
+            {
+                Uri uri = new Uri(endpoint);
+#pragma warning disable SKEXP0010
+                builder.AddOpenAIChatCompletion("gpt-3.5", uri, "no key needed");
+            }
+        }
+        else if (openai_host.Equals("github", StringComparison.OrdinalIgnoreCase))
+        {
+            string endpoint = Environment.GetEnvironmentVariable("GITHUB_MODELS_ENDPOINT");
+            string token = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+            string model = Environment.GetEnvironmentVariable("GITHUB_MODELS_NAME");
+            if (!String.IsNullOrEmpty(endpoint) && !String.IsNullOrEmpty(token) && !String.IsNullOrEmpty(model))
+            {
+                Uri uri = new Uri(endpoint);
+                builder.AddOpenAIChatCompletion(model, uri, token);
+            }
+        }
+        else
+        {
+            string endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+            string model = Environment.GetEnvironmentVariable("AZURE_OPENAI_CHATGPT_DEPLOYMENT");
+            if (!String.IsNullOrEmpty(endpoint) && !String.IsNullOrEmpty(model))
+            {
+                string key = Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY");
+                if (!String.IsNullOrEmpty(key))
+                {
+                    builder.AddAzureOpenAIChatCompletion(model, endpoint, key);
+                }
+                else
+                {
+                }
+                builder.AddAzureOpenAIChatCompletion(model, endpoint, new DefaultAzureCredential());
+            }
+        }
+    }
+}
---
+++ OpenAI-Chat-App-Quickstart-Razor/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+var chatHandler = new ChatHandler();
+
+app.MapPost("/chat", (List<Message> messages) => chatHandler.Chat(messages));
+app.MapPost("/chat/stream", (List<Message> messages) => chatHandler.Stream(messages));
+
+app.Run();
+
+
+app.Run();
